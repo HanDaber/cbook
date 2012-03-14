6e29fefa9cd3bc248fc8e20b2f6fee8bfd2db990
Dan Haber <handaber@gmail.com>
2012-03-13 22:02:04 -0700

    footer styles and js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready( function() {
     var menu_items = $(".menu");
+    var footer = $("footer");
     var w = $(window).width();
     var n = menu_items.length;
 	var x = 0.2;
 	menu_items.width((w-(x*w))/n);
 	$(window).resize( function() {
-		var w = $(window).width();
-		if( w < 600 ) { w = 600; }
-		menu_items.width((w-(x*w))/n);	
+		var this_w = $(window).width();
+		if( this_w < 600 ) { 
+		    this_w = 600; 
+		    footer.css('background-color', '#556688'); 
+		} else if ( this_w > 900 ) {
+		    this_w = 900;
+		    footer.css('background-color', '#003377');
+		} else {
+		    footer.css('background-color', '#003377');
+		}
+		
+		menu_items.width((this_w-(x*this_w))/n);
 	});
         
     var filler_div = $("#filler");
