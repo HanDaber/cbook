a85dabfa4d5c54963c985e57f48d1e85041db95e
Dan Haber <handaber@gmail.com>
2012-03-14 16:11:46 -0700

    troubles with mongodb -34264t5ijfd

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 gem 'sinatra', '1.3.0'
 gem 'thin', '1.2.7'
+gem 'mongo'
 gem 'mongo_mapper'
 gem 'bson_ext'
 gem 'log_buddy'
--- routes/login.rb
+++ routes/login.rb
@@ -8,13 +8,13 @@ get "/login" do
 end
 
 post "/login" do
-  @user = params[:user]
-  @pass = params[:pass]
-  found_user = User.find(@user)
-  if @user == "martha" or @user == "Martha"
-    if @pass == "pass"
-      session[:user] = @user
-      session[:pass] = @pass
+  user = params[:name]
+  pass = params[:pass]
+  #found_user = User.where(:name => @user)
+  valid_user = ["Martha", "pass"]
+  if user == valid_user[0]
+    if pass == valid_user[1]
+      session[:user] = user
       redirect :main
     else
       haml <<"EOT", :layout => :layout
@@ -32,15 +32,17 @@ post "/signup" do
   name = params[:name]
   email = params[:email]
   pass = params[:pass]
-  @new_user = User.create({
-      :user => name,
-      :email => email,
-      :pass => pass,
-      :created_at => Time.now
-  })
-  if @new_user.save
-      session[:user] = @new_user
-      redirect :main
+#  @new_user = User.new()
+#  @new_user.name = name
+#  @new_user.email = email
+#  @new_user.pass = pass
+#  @new_user.created_at = Time.now
+      
+#  if @new_user.save
+  if 1
+    session[:user] = name
+#      session[:user] = @new_user.name
+    redirect :main
   else
     haml <<"EOT", :layout => :layout
 %h1{style:"color:#e00"} Something went wrong, please go back and try again.
