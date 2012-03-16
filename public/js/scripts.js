3ed14d95c4634f414799baaa198abec4d8a635a1
Dan Haber <handaber@gmail.com>
2012-03-16 00:22:48 -0700

    implemented media queries - 9as8dyuweihf

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,24 +6,24 @@ $(document).ready( function() {
     var w = $(window).width();
     var n = menu_items.length;
 	var x = 0.2;
-	
+
 	menu_items.width( (w - (x * w))/n );
-	
+
 	$(window).resize( function() {
 		var this_w = $(window).width();
-		if( this_w < 600 ) { 
-		    this_w = 600; 
-		    footer.css('background-color', '#556688'); 
-		} else if ( this_w > 900 ) {
-		    this_w = 900;
-		    footer.css('background-color', '#003377');
-		} else {
-		    footer.css('background-color', '#003377');
-		}
-		
+        // if( this_w < 600 ) { 
+        //     this_w = 600; 
+        //     footer.css('background-color', '#556688'); 
+        // } else if ( this_w > 900 ) {
+        //     this_w = 900;
+        //     footer.css('background-color', '#003377');
+        // } else {
+        //     footer.css('background-color', '#003377');
+        // }
+
 		menu_items.width( (this_w - (x * this_w))/n );
 
 	});
-    
+	
     $("a.pjax").pjax("#main-container");
 });
