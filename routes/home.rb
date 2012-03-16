522bb2318fac8b0f9435c938f28d8ad551e69780
Dan Haber <handaber@gmail.com>
2012-03-16 03:36:26 -0700

    amend - 1080adc130-26bdc1080cfceda10826cdeac78-52c10826cdd130-104e104-78d78-52c10852e182-26b156-52c130-0a104-78db10826dfcdaa10878abfabc108182eaaebc

---
+++ routes/home.rb
@@ -0,0 +1,40 @@
+get "/" do
+    @title = "Welcome to CollegeBook"
+    if session[:name]
+        name = session[:name]
+        email = session[:email]
+        since = session[:since]
+        pass = session[:pass]
+        @user = {:name => name, :email => email, :since => since}
+        haml :home
+    else
+        haml :index
+    end
+end
+
+get "/home" do
+    if session[:name]
+        name = session[:name]
+        email = session[:email]
+        since = session[:since]
+        @user = {:name => name, :email => email, :since => since}
+        haml :home        
+    else
+        redirect "/"
+    end
+end
+
+# Duct-taped Bulletin Boards for now
+get '/:name' do
+    
+    haml <<"EOT", :layout => !request.pjax?
+%h1{style:"font-size:20pt;"} #{params[:name]} Bulletin Board
+%h3 Testing...
+%p= Time.now.strftime("Debug: loaded@ %Y/%m/%d %H:%M:%S.")
+EOT
+
+end
+
+not_found do
+    haml :e404
+end
