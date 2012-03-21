e7bfc326789a41b4ea4679496d5f9102836d4520
Dan Haber <handaber@gmail.com>
2012-03-20 21:20:47 -0700

    ammmmendicled - rt483498tqiur3hgiuqhet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,6 +14,32 @@ $(document).ready( function() {
 		menu_items.width( (this_w - (x * this_w))/n );
 	});
 	
+    // http://webdesign.maratz.com/lab/fancy-checkboxes-and-radio-buttons/jquery.html:
+    // function setupLabel() {
+    //             if ($('.label_check input').length) {
+    //                 $('.label_check').each(function(){ 
+    //                     $(this).removeClass('c_on');
+    //                 });
+    //                 $('.label_check input:checked').each(function(){ 
+    //                     $(this).parent('label').addClass('c_on');
+    //                 });                
+    //             };
+    //             if ($('.label_radio input').length) {
+    //                 $('.label_radio').each(function(){ 
+    //                     $(this).removeClass('r_on');
+    //                 });
+    //                 $('.label_radio input:checked').each(function(){ 
+    //                     $(this).parent('label').addClass('r_on');
+    //                 });
+    //             };
+    //         };
+    //      $('body').addClass('has-js');
+    //         $('.label_check, .label_radio').click(function(){
+    //             setupLabel();
+    //         });
+    //         setupLabel();
+        
+        
 	// Select which container to load pjax responses into
     // $("a.pjax").pjax("#main-container");
     // $(".posts").pjax("#middle");
