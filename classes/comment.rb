553d0f5987e58607b3a6d6551ab7384cf8c1e03f
Dan Haber <handaber@gmail.com>
2012-05-03 14:45:47 -0700

    tweak - 34tg

--- classes/comment.rb
+++ classes/comment.rb
@@ -1,7 +1,6 @@
 class Comment < CollegeBook
     
     key :text,      String, required: true
-    key :posted_by, String, required: true
     
     belongs_to :post
     belongs_to :user, :as => :posted_by
